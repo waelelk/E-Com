af664e0f5f3c5235c8fd09d06aa606846502c761
Wael Elk <codewithwael@gmail.com>
2021-06-20 16:44:14 +0100

    Link to MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,17 @@
-rquire('dotenv').config();
-//db
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connection Success");
+    }catch (error){
+        console.error("MongoDB connection failed");
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
