2248e0fcd3a122ead59a97253344a76b564e0fbc
Wael Elk <codewithwael@gmail.com>
2021-06-21 10:16:33 +0100

    Front-end (empty)
    empty layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Navbar */}
+      {/* Sidedrawer */}
+      {/* Backdrop */}
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route exact path="/product/:id" component={ProductScreen}/>
+          <Route exact path="/cart" component={CartScreen}/>
+        </Switch>
+      </main>
+
+      {/* Home Screen */}
+
+      {/* Product Screen */}
+
+      {/* Card Screen */}
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <div classname='navbar'>
+            {/* Logo */}
+
+        </div>
+    )
+}
+
+export default Navbar
