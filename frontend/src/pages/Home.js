79ed1c7875354f4f0f007b8347c71406064a8ab3
Wael Elk <codewithwael@gmail.com>
2021-06-23 10:30:13 +0100

    Responsive Navbar using Tailwind

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import * as React from "react";
+
 
 function Home() {
     return (
-        <> 
-        
+        <>
         </>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
+      extend: {
+          spacing: {
+              128: '32rem',
+          },
+      },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
